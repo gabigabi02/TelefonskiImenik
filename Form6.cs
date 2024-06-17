0f433443ba9c2d91983a77568cc132e95109076c
gabigabi02 <gabriela.dilber@skole.hr>
2024-06-17 10:42:45 +0200

    telefonskiImenik

--- Form6.cs
+++ Form6.cs
@@ -96,7 +96,6 @@ namespace TelefonskiImenik
                 if (insertIndex < 0)
                     insertIndex = ~insertIndex; 
 
-
                 for (int i = index; i > insertIndex; i--)
                 {
                     ime[i] = ime[i - 1];
